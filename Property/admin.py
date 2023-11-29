a970d0d5570c40d9436d586b66a623ef8535ed87
AbhiMisRaw <sumitmm9506555798@gmail.com>
2023-11-29 09:45:16 -0800

    added a feature to uselect the location on map direclty using map

--- Property/admin.py
+++ Property/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 
 from .models import Property
 
-from .forms import PropertyForm
+# from .forms import PropertyForm
 
 # Register your models here.
 
 
-class PropertyAdmin(admin.ModelAdmin):
-    form = PropertyForm
+# class PropertyAdmin(admin.ModelAdmin):
+#     form = PropertyForm
 
 
-admin.site.register(Property, PropertyAdmin)
+admin.site.register(Property)  # , PropertyAdmin
--- Property/models.py
+++ Property/models.py
@@ -5,8 +5,13 @@ from django.contrib.gis.geos import Point
 
 # Create your models here.
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Property(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=150)
     description = models.TextField(null=True, blank=True)
 
@@ -45,7 +50,11 @@ class Property(models.Model):
     cctv = models.BooleanField(default=False)
     parking = models.BooleanField(default=False)
     date_posted = models.DateTimeField(default=timezone.now)
-    location = models.PointField(blank=True, null=True, srid=4326)
+    # location = models.PointField(blank=True, null=True, srid=4326)
+
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+
     picture1 = models.ImageField(blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
     picture2 = models.ImageField(blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
     picture3 = models.ImageField(blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
